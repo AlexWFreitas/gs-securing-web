62c63eaaa1156aa2aa5174dcb4d775ceae4542b6
Alex Wong de Freitas <otaru1921@gmail.com>
2021-11-12 10:43:21 -0300

    Finished the tutorial, changed withDefaultPasswordEncoder to a custom Password Encoder because the default is deprecated.

--- initial/src/main/java/com/example/securingweb/SecuringWebApplication.java
+++ initial/src/main/java/com/example/securingweb/SecuringWebApplication.java
@@ -2,12 +2,20 @@ package com.example.securingweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SecuringWebApplication {
 
 	public static void main(String[] args) throws Throwable {
 		SpringApplication.run(SecuringWebApplication.class, args);
+	}
+
+	@Bean
+	public PasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
